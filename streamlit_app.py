36af535fbd03d9ffb01fc6d1433e0eed4684e5a1
snwflkworkshop <158742156+snwflkworkshop@users.noreply.github.com>
2024-02-03 13:06:55 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import pandas;
 
 #INIT
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 #PAGE BUIDING
 streamlit.title("My Parents New Healthy Dinner")
@@ -15,4 +14,5 @@ streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
