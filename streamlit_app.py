614b69592479a3fb28037fd23961c5f19ee53d29
snwflkworkshop <158742156+snwflkworkshop@users.noreply.github.com>
2024-02-03 13:09:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,5 +15,5 @@ streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avocado","Strawberries"])
 streamlit.dataframe(my_fruit_list)
