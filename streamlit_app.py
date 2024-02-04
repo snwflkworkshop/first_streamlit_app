14bf69b8c8e3a118b87ca1d6c6d010bf66b2413f
snwflkworkshop <158742156+snwflkworkshop@users.noreply.github.com>
2024-02-04 17:03:58 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 import streamlit;
 import pandas;
+import requests;
 
 #INIT
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 #PAGE BUIDING
 streamlit.title("My Parents New Healthy Dinner")
@@ -18,3 +22,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avocado","Strawberries"])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
+
+streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response.json())
